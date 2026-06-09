34dbe7696f1ecd39c0ee0f4682350ee9d0fee3c3
t9a <200544443+t9a-dev@users.noreply.github.com>
2026-06-09 20:55:13 +0900

    add smart-split

---
+++ nvim/lua/plugins/smart-split.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        mode = { "n", "t" },
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        mode = { "n", "t" },
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        mode = { "n", "t" },
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        mode = { "n", "t" },
+      },
+    },
+    opts = {},
+  },
+}
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              ["<C-h>"] = { "smart_split_left", mode = { "i", "n" } },
+              ["<C-j>"] = { "smart_split_down", mode = { "i", "n" } },
+              ["<C-k>"] = { "smart_split_up", mode = { "i", "n" } },
+              ["<C-l>"] = { "smart_split_right", mode = { "i", "n" } },
+            },
+          },
+          list = {
+            keys = {
+              ["<C-h>"] = "smart_split_left",
+              ["<C-j>"] = "smart_split_down",
+              ["<C-k>"] = "smart_split_up",
+              ["<C-l>"] = "smart_split_right",
+            },
+          },
+        },
+        actions = {
+          smart_split_left = function()
+            require("smart-splits").move_cursor_left()
+          end,
+          smart_split_down = function()
+            require("smart-splits").move_cursor_down()
+          end,
+          smart_split_up = function()
+            require("smart-splits").move_cursor_up()
+          end,
+          smart_split_right = function()
+            require("smart-splits").move_cursor_right()
+          end,
+        },
+      },
+    },
+  },
+}
