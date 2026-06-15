d55aded14d3b03d2a414b8e10e083d84619f3f27
t9a <200544443+t9a-dev@users.noreply.github.com>
2026-06-15 14:52:11 +0900

    update

---
+++ nvim/lua/plugins/checkmate.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    -- markdownでのtodo管理支援
+    "bngarren/checkmate.nvim",
+    ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+    opts = {
+      -- files = { "*.md" }, -- any .md file (instead of defaults)
+    },
+  },
+}
