f17beacae1214e4c42f6fd3909fd4b5903f42c5e
t9a <200544443+t9a-dev@users.noreply.github.com>
2026-06-15 14:53:41 +0900

    update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- markdownでスペルチェックを無効化
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function(_)
+    vim.opt_local.spell = false
+    -- vim.diagnostic.enable(false, { bufnr = args.buf })
+  end,
+})
