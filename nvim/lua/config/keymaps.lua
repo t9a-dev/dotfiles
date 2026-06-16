4c2eb2e3fa70e6fce1955121839ef74ec2c19a3c
t9a <200544443+t9a-dev@users.noreply.github.com>
2026-06-17 08:59:07 +0900

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+-- 意図せずマクロ記録モードに入って鬱陶しいので<Leader>経由で使うように
+-- Disable accidental macro recording on q
+vim.keymap.set("n", "q", "<Nop>", {
+  desc = "Disable accidental macro recording",
+})
+
+-- Keep macro recording available on Q
+vim.keymap.set("n", "Q", "q", {
+  desc = "Macro: start/stop recording",
+})
+
+-- Expose macro recording in <Leader> menu
+vim.keymap.set("n", "<Leader>Q", "q", {
+  desc = "Macro: start/stop recording",
+})
